1601981769da84b1f3f2ea2964e4bd92ab688120
Ujjawal Anand <uswalanand1999@gmail.com>
2023-10-04 18:19:30 +0000

    skip-ci

--- target/compiled/dbtlearn/models/dim/dim_hosts_cleansed.sql
+++ target/compiled/dbtlearn/models/dim/dim_hosts_cleansed.sql
@@ -1,6 +1,15 @@
 
-WITH src_hosts as (
-    SELECT * FROM AIRBNB.DEV.src_hosts
+WITH  __dbt__cte__src_hosts as (
+WITH raw_hosts AS ( 
+  SELECT * FROM AIRBNB.raw.raw_hosts
+)
+SELECT
+  id AS host_id,
+  NAME AS host_name, is_superhost, created_at, updated_at
+FROM
+  raw_hosts
+), src_hosts as (
+    SELECT * FROM __dbt__cte__src_hosts
 )
 SELECT
     host_id,
--- target/compiled/dbtlearn/models/dim/dim_listings_cleansed.sql
+++ target/compiled/dbtlearn/models/dim/dim_listings_cleansed.sql
@@ -1,6 +1,22 @@
 
-WITH src_listings AS (
-    select * FROM AIRBNB.DEV.src_listings
+WITH  __dbt__cte__src_listings as (
+WITH raw_listings AS (
+    SELECT * FROM AIRBNB.raw.raw_listings
+)
+SELECT 
+    id as listing_id,
+    name as listing_name,
+    listing_url,
+    room_type,
+    minimum_nights,
+    host_id,
+    price as price_str,
+    created_at,
+    updated_at
+FROM
+    raw_listings
+), src_listings AS (
+    select * FROM __dbt__cte__src_listings
 )
 SELECT 
     listing_id,
